db79ecb32a1cbe778684b5ea9a24d5dc25652222
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-01 19:55:14 +0800

    [OOpratice][AnnA]feat: practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,26 @@
 package practice07;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,33 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age){
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass){
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if(getKlass()!=null){
+            return super.introduce() + String.format(" I am a Teacher. I teach %s.", klass.getDisplayName());
+        } else{
+            return super.introduce() + String.format(" I am a Teacher. I teach No Class.");
+        }
+    }
+    public String introduceWith(Student student){
+        if(klass.getNumber() == student.getKlass().getNumber()){
+            return "My name is "+ getName() + ". I am " + getAge() +" years old. I am a Teacher. I teach " + student.getName() + ".";
+        } else {
+            return "My name is "+ getName() + ". I am " + getAge() +" years old. I am a Teacher. I don't teach " + student.getName() + ".";
+        }
+    }
 }
