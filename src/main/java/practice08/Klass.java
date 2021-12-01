45283cbd27d583a39debc74947348d69712d47e4
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-01 20:23:43 +0800

    [OOpractise][Anna]feat: Pracrise08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,40 @@
 package practice08;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> students;
+
+    public Klass(int number){
+        this.number = number;
+        students = new ArrayList<Student>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student){
+        if(students.contains(student)){
+            this.leader = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student){
+        students.add(student);
+    }
+
 }
